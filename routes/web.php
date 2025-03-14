4e15fe5a95f84cf9e01711dc9fd7214ee7afa78d
Stole Krshkov <krshkovstole@gmail.com>
2025-03-15 00:08:01 +0100

    feat: auth views init

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::permanentRedirect('/', '/home');
+Route::view('/home', 'home')->middleware(['auth'])->name('home');
