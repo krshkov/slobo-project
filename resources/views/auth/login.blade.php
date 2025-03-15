92b33792975bed2adc0666d106d4aa607bebb30c
Slobodan Sharenkovski <sharenkovskislobodan@gmail.com>
2025-03-15 15:49:54 +0100

    chore: adjust login colors and base

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,31 +2,34 @@
     <x-alerts.errors />
     <x-alerts.status />
 
-    <div class="px-4 py-4 sm:mx-auto sm:w-full sm:max-w-lg sm:shadow sm:bg-gray-50 sm:rounded-lg sm:px-10">
-        <form method="POST" action="{{ route('login') }}" class="mb-4 space-y-2">
+    <div class="px-4 py-4 sm:mx-auto sm:w-full sm:max-w-lg sm:px-10">
+        <h3>Welcome back</h3>
+        <p>Register with your account</p>
+
+        <form method="POST" action="{{ route('login') }}" class="my-4 space-y-2">
             @csrf
 
-            <div class="space-y-1">
-                <label for="email" class="text-gray-500">{{ __('Email') }}</label>
-                <input type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="email"
-                    class="w-full px-3 py-2 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline focus:border-blue-300" />
+            <div class="space-y-1 relative">
+                <x-icon-mail class="absolute left-3 top-0 bottom-0 my-auto" />
+                <input type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="email" placeholder="e-mail"
+                    class="w-full px-4 py-3 pl-10 transition duration-150 ease-in-out border border-gray-300 rounded-xl appearance-none focus:outline-none focus:shadow-outline focus:border-orange-300" />
             </div>
 
-            <div class="space-y-1">
-                <label for="password" class="text-gray-500">{{ __('Password') }}</label>
-                <input type="password" name="password" required autocomplete="current-password"
-                    class="w-full px-3 py-2 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline focus:border-blue-300" />
+            <div class="space-y-1 relative">
+                <x-icon-lock class="absolute left-3 top-0 bottom-0 my-auto" />
+                <input type="password" name="password" required autocomplete="current-password" placeholder="password"
+                    class="w-full px-4 py-3 pl-10 transition duration-150 ease-in-out border border-gray-300 rounded-xl appearance-none focus:outline-none focus:shadow-outline focus:border-orange-300" />
             </div>
 
             <div class="flex items-end justify-between py-2">
                 <div class="flex items-center">
                     <input type="checkbox" name="remember"
-                        class="w-4 h-4 text-blue-500 transition duration-150 ease-in-out focus:outline-none focus:shadow-outline focus:border-blue-300">
+                        class="w-4 h-4 text-orange-500 transition duration-150 ease-in-out focus:outline-none focus:shadow-outline focus:border-orange-300">
                     <label for="remember_me" class="ml-2 text-gray-500 ">{{ __('Remember me') }}</label>
                 </div>
 
                 <button type="submit"
-                    class="flex justify-center w-1/2 py-2 text-white transition duration-300 ease-in-out bg-blue-500 border border-blue-600 rounded-md hover:bg-blue-600 focus:outline-none focus:border-blue-700 focus:shadow-outline active:bg-blue-700">
+                    class="flex justify-center w-1/2 py-2 text-white transition duration-300 ease-in-out bg-orange-500 border border-orange-600 rounded-md hover:bg-orange-600 focus:outline-none focus:border-orange-700 focus:shadow-outline active:bg-orange-700">
                     {{ __('Login') }}
                 </button>
             </div>
@@ -34,13 +37,13 @@
             <div class="flex items-center justify-between">
                 @if (Route::has('password.request'))
                 <a href="{{ route('password.request') }}"
-                    class="text-blue-600 transition duration-150 ease-in-out hover:text-blue-400 focus:outline-none focus:underline">
+                    class="text-orange-600 transition duration-150 ease-in-out hover:text-orange-400 focus:outline-none focus:underline">
                     {{ __('Forgot your password?') }}
                 </a>
                 @endif
                 @if (Route::has('register'))
                 <a href="{{ route('register') }}"
-                    class="text-blue-600 transition duration-150 ease-in-out hover:text-blue-400 focus:outline-none focus:underline">
+                    class="text-orange-600 transition duration-150 ease-in-out hover:text-orange-400 focus:outline-none focus:underline">
                     {{ __('Not registered?') }}
                 </a>
                 @endif
