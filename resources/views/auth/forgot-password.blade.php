a8bba4419727bb48668c9e7ab231dee3a9ff95d0
Stole Krshkov <krshkovstole@gmail.com>
2025-03-15 16:01:20 +0100

    chore: adjust forgot-password colors and base

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -14,20 +14,20 @@
             <div class="flex items-start space-x-2">
                 <label for="email" class="text-gray-500">{{ __('Email') }}</label>
                 <input type="email" name="email" value="{{ old('email') }}" required autofocus
-                    class="w-full px-3 py-2 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline focus:border-blue-300" />
+                    class="w-full px-3 py-2 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline focus:border-orange-300" />
             </div>
 
             <div class="flex items-end justify-between">
                 @if (Route::has('register'))
                 <a href="{{ route('register') }}"
-                    class="text-blue-600 transition duration-150 ease-in-out hover:text-blue-400 focus:outline-none focus:underline">
+                    class="text-orange-600 transition duration-150 ease-in-out hover:text-orange-400 focus:outline-none focus:underline">
                     {{ __('Not registered?') }}
                 </a>
                 @endif
 
                 <div class="flex justify-end pt-4">
                     <button type="submit"
-                        class="flex justify-center px-2 py-2 text-white transition duration-300 ease-in-out bg-blue-500 border border-blue-600 rounded-md hover:bg-blue-600 focus:outline-none focus:border-blue-700 focus:shadow-outline active:bg-blue-700">
+                        class="flex justify-center px-2 py-2 text-white transition duration-300 ease-in-out bg-orange-500 border border-orange-600 rounded-md hover:bg-orange-600 focus:outline-none focus:border-orange-700 focus:shadow-outline active:bg-orange-700">
                         {{ __('Email Password Reset Link') }}
                     </button>
                 </div>
