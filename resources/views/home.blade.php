3faf0661a921a76efc3b542e9690a7f0a39ad5ad
Stole Krshkov <krshkovstole@gmail.com>
2025-03-16 15:06:31 +0100

    feat: introduce home for client and handyman separately

--- resources/views/home.blade.php
+++
@@ -1,3 +0,0 @@
-<x-layouts.guest>
-    <h1 class="text-primary ms-52">Hello, Tailwind in Laravel!</h1>
-</x-layouts.guest>
---
+++ resources/views/home_client.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.guest>
+    <h1>Client Home</h1>
+</x-layouts.guest>
